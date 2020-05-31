92ee483e53b25a18d74a156ad6c022f21609314c
jianganglu <1055375563@qq.com>
2020-05-31 17:15:53 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-npm run dev
+#!/bin/bash
+git checkout dev
+npm run build:prod
+# htmldir = "D:/"
+# uploadbasedir = "${htmldir}/hooks"
+# uploaddir = "${uploadbasedir}/v1.0.0"
+# scp -r ./dist/. txcloud:${uploaddir}
+scp -r ./dist/. txcloud: "D:/hooks/v1.0.0"
+exit
+eeooff
+echo done
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const body = require('koa-better-body');
 
-const ch = require('child_process');
+const { execFile } = require('child_process');
 
 let app = new Koa();
 
@@ -12,18 +12,25 @@ app.use(async ctx => {
   // console.log(url); // 请求url
   console.log(ctx.request.fields); // 携带过来的数据
 
-  let worker = ch.spawn('git', ['pull'], {
-    cwd: '/',
-    stdio: 'inherit'
+  execFile('deploy.sh', { shell: true }, (err, stdout, stderr) => {
+    if (err) {
+      throw err;
+    }
+    console.log(stdout);
   });
 
-  worker.on('close', (code) => {
-    if (code === 0) {
-      console.log('pull success!');
-    } else {
-      console.log('pull error!');
-    }
-  });
+  // let worker = ch.spawn('git', ['pull'], {
+  //   cwd: '/',
+  //   stdio: 'inherit'
+  // });
+
+  // worker.on('close', (code) => {
+  //   if (code === 0) {
+  //     console.log('pull success!');
+  //   } else {
+  //     console.log('pull error!');
+  //   }
+  // });
 
   ctx.body = 'ok';
 });
